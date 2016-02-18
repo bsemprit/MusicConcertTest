233d3fae56a92ecad4466b8b69374e86da2c1d44
Bianca Semprit <bsemprit@gmail.com>
2016-02-18 10:27:31 -0500

    Debugging

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,15 @@
 
 
 goddess_tour = Concert.create(artist: "BANKS", venue: "Grand Central", city: "Miami", date: DateTime.new(2016, 6, 1), price: 30.00, description: "Computational processes are abstract beings that inhabit computers. As they evolve, processes manipulate other abstract things called data. The evolution of a process is directed by a pattern of rules called a program. People create programs to direct processes. In effect, we conjure the spirits of the computer with our spells.")
-dog_days_tour = Concert.create(artist: "Florence and the Machine", venue: "American Airlines Arena", city: "Miami", date: DateTime.new(2016, 2, 6), price: 100.00, description: "Computational processes are abstract beings that inhabit computers. As they evolve, processes manipulate other abstract things called data. The evolution of a process is directed by a pattern of rules called a program. People create programs to direct processes. In effect, we conjure the spirits of the computer with our spells.")
+dog_days_tour = Concert.create(artist: "Florence and the Machine", venue: "American Airlines Arena", city: "Miami", date: DateTime.new(2016, 2, 25), price: 100.00, description: "Computational processes are abstract beings that inhabit computers. As they evolve, processes manipulate other abstract things called data. The evolution of a process is directed by a pattern of rules called a program. People create programs to direct processes. In effect, we conjure the spirits of the computer with our spells.")
 tropical_tour = Concert.create(artist: "Thomas Jack", venue: "Bardot", city: "Miami", date: DateTime.new(2016, 5, 1), price: 10.00, description: "Computational processes are abstract beings that inhabit computers. As they evolve, processes manipulate other abstract things called data. The evolution of a process is directed by a pattern of rules called a program. People create programs to direct processes. In effect, we conjure the spirits of the computer with our spells.")
-swedish_tour = Concert.create(artist: "Joris Voorn", venue: "Electric Pickle", city: "Miami", date: DateTime.new(2016, 2, 15), price: 20.00, description: "Computational processes are abstract beings that inhabit computers. As they evolve, processes manipulate other abstract things called data. The evolution of a process is directed by a pattern of rules called a program. People create programs to direct processes. In effect, we conjure the spirits of the computer with our spells.")
+swedish_tour = Concert.create(artist: "Joris Voorn", venue: "Electric Pickle", city: "Miami", date: DateTime.new(2016, 2, 22), price: 20.00, description: "Computational processes are abstract beings that inhabit computers. As they evolve, processes manipulate other abstract things called data. The evolution of a process is directed by a pattern of rules called a program. People create programs to direct processes. In effect, we conjure the spirits of the computer with our spells.")
 
+puts "yo"
+puts goddess_tour.errors.full_messages
+puts dog_days_tour.errors.full_messages
+puts tropical_tour.errors.full_messages
+puts swedish_tour.errors.full_messages
 
 # comment1 = goddess_tour.Comment.create(name: "Bobby Flay", email: "hestheflay@flay.com", body: "This place is the best!!")
 # comment2 = goddess_tour.Comment.create(name: "Anais", email: "shestheflay@flay.com", body: "This place is the best!!")
